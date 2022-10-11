d775d00b2275a642309ad35aa7ea2101f4e7873d
Abrar Hussain <65803677+AbrarTunio@users.noreply.github.com>
2022-10-11 15:58:23 +0500

    Add files via upload

---
+++ exercise4.js
@@ -0,0 +1,157 @@
+document.querySelectorAll("input").forEach((element) => {
+  element.setAttribute("class", "text-center");
+});
+
+document.querySelectorAll("div.row").forEach((element) => {
+  element.classList.add("class", "text-dark");
+});
+
+document.querySelectorAll("button.btn").forEach((element) => {
+  element.classList.add("class", "mt-2");
+});
+
+fillOne = document.querySelector("#fillOne");
+statOne = document.querySelector("#statOne");
+
+function questionOne() {
+  if (fillOne.value == "src") {
+    statOne.innerText = "Correct";
+    statOne.setAttribute("class", "row alert alert-success");
+  } else {
+    statOne.innerText = "Wrong";
+    statOne.setAttribute("class", "row alert alert-danger");
+  }
+}
+
+fillTwo = document.querySelector("#fillTwo");
+statTwo = document.querySelector("#statTwo");
+
+function questionTwo() {
+  if (fillTwo.value == "controls") {
+    statTwo.innerText = "Correct";
+    statTwo.setAttribute("class", "row alert alert-success");
+  } else {
+    statTwo.innerText = "Wrong";
+    statTwo.setAttribute("class", "row alert alert-danger");
+  }
+}
+
+fillThr = document.querySelector("#fillThr");
+statThr = document.querySelector("#statThr");
+
+function questionThr() {
+  if (fillThr.value == "loop") {
+    statThr.innerText = "Correct";
+    statThr.setAttribute("class", "row alert alert-success");
+  } else {
+    statThr.innerText = "Wrong";
+    statThr.setAttribute("class", "row alert alert-danger");
+  }
+}
+
+fillFou = document.querySelector("#fillFou");
+fillFour = document.querySelector("#fillFour");
+fillFour1 = document.querySelector("#fillFour1");
+fillFour2 = document.querySelector("#fillFour2");
+statFou = document.querySelector("#statFou");
+
+function questionFou() {
+  if (
+    fillFou.value == "<source" &&
+    fillFour.value == ">" &&
+    fillFour1.value == "<source" &&
+    fillFour2.value == ">"
+  ) {
+    statFou.innerText = "Correct";
+    statFou.setAttribute("class", "row alert alert-success");
+  } else {
+    statFou.innerText = "Wrong";
+    statFou.setAttribute("class", "row alert alert-danger");
+  }
+}
+
+fillFiv = document.querySelector("#fillFiv");
+fillFive = document.querySelector("#fillFive");
+fillFive1 = document.querySelector("#fillFive1");
+statFiv = document.querySelector("#statFiv");
+
+function questionFiv() {
+  if (
+    fillFiv.value == "<video" &&
+    fillFive.value == ">" &&
+    fillFive1.value == "</video>"
+  ) {
+    statFiv.innerText = "Correct";
+    statFiv.setAttribute("class", "row alert alert-success");
+  } else {
+    statFiv.innerText = "Wrong";
+    statFiv.setAttribute("class", "row alert alert-danger");
+  }
+}
+
+fillSix = document.querySelector("#fillSix");
+fillSix1 = document.querySelector("#fillSix1");
+statSix = document.querySelector("#statSix");
+
+function questionSix() {
+  if (fillSix.value == "autoplay" && fillSix1.value == "</video>") {
+    statSix.innerText = "Correct";
+    statSix.setAttribute("class", "row alert alert-success");
+  } else {
+    statSix.innerText = "Wrong";
+    statSix.setAttribute("class", "row alert alert-danger");
+  }
+}
+
+fillSev = document.querySelector("#fillSev");
+statSev = document.querySelector("#statSev");
+
+function questionSev() {
+  if (fillSev.value == "<track") {
+    statSev.innerText = "Correct";
+    statSev.setAttribute("class", "row alert alert-success");
+  } else {
+    statSev.innerText = "Wrong";
+    statSev.setAttribute("class", "row alert alert-danger");
+  }
+}
+
+fillEig = document.querySelector("#fillEig");
+statEig = document.querySelector("#statEig");
+
+function questionEig() {
+  if (fillEig.value == "default") {
+    statEig.innerText = "Correct";
+    statEig.setAttribute("class", "row alert alert-success");
+  } else {
+    statEig.innerText = "Wrong";
+    statEig.setAttribute("class", "row alert alert-danger");
+  }
+}
+
+fillNin = document.querySelector("#fillNin");
+fillNine = document.querySelector("#fillNine");
+statNin = document.querySelector("#statNin");
+
+function questionNin() {
+  if (fillNin.value == "</li>" && fillNine.value == "</ul>") {
+    statNin.innerText = "Correct";
+    statNin.setAttribute("class", "row alert alert-success");
+  } else {
+    statNin.innerText = "Wrong";
+    statNin.setAttribute("class", "row alert alert-danger");
+  }
+}
+
+fillTen = document.querySelector("#fillTen");
+statTen = document.querySelector("#statTen");
+
+function questionTen() {
+  if (fillTen.value == "3") {
+    statTen.innerText = "Correct";
+    statTen.setAttribute("class", "row alert alert-success");
+  } else {
+    statTen.innerText = "Wrong";
+    statTen.setAttribute("class", "row alert alert-danger");
+  }
+}
